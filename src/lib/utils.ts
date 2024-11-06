2ddecb0782f3145baeacff1612c1237c16e8388c
Ycow <skypnal12@gmail.com>
2024-11-06 18:22:47 +0900

    feat: fn Comment Add

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ *
+ * @param inputs 합칠 클래스들
+ * @returns 테일윈드용 merge ClassValue
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
