e9957d511ec408c4a58e76e5cfd03dc2cec827c8
Ycow <skypnal12@gmail.com>
2024-11-23 15:39:50 +0900

    chore: assets add and folder path fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-import createMDX from "@next/mdx";
+import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
+	// Configure `pageExtensions` to include markdown and MDX files
+	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+	// Optionally, add any other Next.js config below
 };
 
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-  extension: /\.mdx?$/,
+	// Add markdown plugins here, as desired
+	extension: /\.mdx?$/,
 });
 
 // Merge MDX config with Next.js config
