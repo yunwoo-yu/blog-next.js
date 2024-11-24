6e24db2173c379568242aa53c1e16f89e8daddde
Ycow <skypnal12@gmail.com>
2024-11-24 21:19:45 +0900

    feat: add to PostListType Thumbnail

--- src/components/posts/PostListType.tsx
+++ src/components/posts/PostListType.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { CompileMdxTypes } from '@/types/common.types';
@@ -12,19 +13,24 @@ interface PostListTypeProps {
 const PostListType = ({ data, category, slug }: PostListTypeProps) => {
 	return (
 		<li className="flex cursor-pointer flex-col border-t border-gray-400 px-2 py-5">
-			<Link href={`/blog/posts/${category}/${slug}`}>
-				<p className="text-xl">{data.title}</p>
-				<div className="my-1 flex text-xs">
-					<span className="text-gray-400">{data.createdAt}</span>
-					<div>
-						{data.tags.map((tag, idx) => (
-							<span className={cn('text-gray-500', !idx ? 'ml-2' : 'ml-1')} key={slug + tag}>
-								#{tag}
-							</span>
-						))}
+			<Link href={`/blog/posts/${category}/${slug}`} className="flex justify-between">
+				<div>
+					<p className="text-xl">{data.title}</p>
+					<p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{data.description}</p>
+					<div className="mt-3 flex text-xs">
+						<span className="text-gray-400">{data.createdAt}</span>
+						<div>
+							{data.tags.map((tag, idx) => (
+								<span className={cn('text-gray-500', !idx ? 'ml-2' : 'ml-1')} key={slug + tag}>
+									#{tag}
+								</span>
+							))}
+						</div>
 					</div>
 				</div>
-				<p className="mt-1 text-sm text-gray-500 dark:text-gray-300">{data.description}</p>
+				<div className="relative hidden aspect-video w-full max-w-36 sm:block">
+					<Image src={data.thumbnail} alt="post thumbnail" fill className="object-cover" />
+				</div>
 			</Link>
 		</li>
 	);
