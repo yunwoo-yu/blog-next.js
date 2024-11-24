ee719fe0a4f195c232f1f425f123f710a4e32c88
Ycow <skypnal12@gmail.com>
2024-11-24 21:20:04 +0900

    feat: add PostCardType Component

--- src/components/posts/PostCardType.tsx
+++ src/components/posts/PostCardType.tsx
@@ -1,3 +1,7 @@
+import { CalendarDays } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
 import { CompileMdxTypes } from '@/types/common.types';
 
 interface PostCardTypeProps {
@@ -6,11 +10,25 @@ interface PostCardTypeProps {
 	slug: string;
 }
 
-const PostCardType = ({ data }: PostCardTypeProps) => {
+const PostCardType = ({ data, category, slug }: PostCardTypeProps) => {
 	return (
-		<li>
-			{/* <Link></Link> */}
-			{data.title}
+		<li className="border border-border transition dark:hover:border-destructive">
+			<Link href={`/blog/posts/${category}/${slug}`}>
+				<div className="relative aspect-video w-full">
+					<Image src={data.thumbnail} alt="post thumbnail" fill className="object-cover" />
+				</div>
+				<div className="p-4">
+					<p className="text-xl">
+						{data.title}
+						<span></span>
+					</p>
+					<p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{data.description}</p>
+					<p className="mt-2 flex items-center text-xs text-gray-400">
+						<CalendarDays className="mr-1 size-4" />
+						{data.createdAt}
+					</p>
+				</div>
+			</Link>
 		</li>
 	);
 };
