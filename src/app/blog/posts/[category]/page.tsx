2df84439b67fa84e8d5e555b39315ce948935435
Ycow <skypnal12@gmail.com>
2024-11-25 21:24:12 +0900

    feat: Posts with Category page add, Static Params add

--- src/app/blog/posts/[category]/page.tsx
+++ src/app/blog/posts/[category]/page.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import CategoryVerticalList from '@/components/common/CategoryVerticalList';
+import BlogPosts from '@/components/posts/BlogPosts';
+import { getAllPosts, getAllPostsPath, getCategoryList } from '@/utils/post-utils';
 
 interface Params {
 	params: Promise<{ category: string }>;
 }
 
+export async function generateStaticParams() {
+	const category = await getCategoryList();
+
+	return category.categoryNameList.map(categoryName => ({ category: categoryName.label }));
+}
+
 const PostsPageWithCategory = async ({ params }: Params) => {
 	const { category } = await params;
+	const postsPaths = getAllPostsPath(category);
+	const posts = await getAllPosts(postsPaths);
+	const categoryData = await getCategoryList();
 
-	console.log(category);
-
-	return <div>PostsPageWithCategory</div>;
+	return (
+		<section className="relative mx-auto flex max-w-6xl gap-5 px-5 py-20">
+			<h1 className="sr-only">Ycow Blog</h1>
+			<CategoryVerticalList categoryData={categoryData} category={category} />
+			<BlogPosts posts={posts} />
+		</section>
+	);
 };
 
 export default PostsPageWithCategory;
