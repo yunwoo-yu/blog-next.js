de73aba6abb2deada91ea49ed17f9060d30d91df
Ycow <skypnal12@gmail.com>
2024-11-27 23:28:30 +0900

    refactor: 디테일 페이지 컴포넌트 분리

--- src/app/blog/posts/[category]/[slug]/page.tsx
+++ src/app/blog/posts/[category]/[slug]/page.tsx
@@ -1,8 +1,7 @@
-import { CalendarDays } from 'lucide-react';
-
 import CustomMDXRemote from '@/components/common/CustomMDXRemote';
+import Comments from '@/components/postDetail/Comments';
+import PostDetailHeader from '@/components/postDetail/PostDetailHeader';
 import { getPostDetail } from '@/utils/post-utils';
-import { cn } from '@/utils/utils';
 
 interface Params {
 	params: Promise<{ category: string; slug: string }>;
@@ -14,25 +13,10 @@ const PostDetailPage = async ({ params }: Params) => {
 
 	return (
 		<>
-			<section className="mx-auto max-w-5xl border-b border-border px-5 py-10">
-				<h1 className="text-3xl text-primary">{frontmatter.title}</h1>
-				<div className="mt-2 flex justify-between text-sm text-gray-400">
-					<span>{frontmatter.description}</span>
-					<span className="flex items-center">
-						<CalendarDays className="mr-1 size-4" />
-						{frontmatter.createdAt}
-					</span>
-				</div>
-				<div className="mt-2">
-					{frontmatter.tags.map((tag, idx) => (
-						<span className={cn('text-sm text-gray-500', idx && 'ml-1')} key={slug + tag}>
-							#{tag}
-						</span>
-					))}
-				</div>
-			</section>
+			<PostDetailHeader frontmatter={frontmatter} />
 			<section className="prose mx-auto max-w-4xl px-5 py-10 dark:prose-invert">
 				<CustomMDXRemote source={source} />
+				<Comments />
 			</section>
 		</>
 	);
