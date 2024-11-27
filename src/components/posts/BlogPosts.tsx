6f26e2f4c971622798b724a6268e5b987e7bd8a2
Ycow <skypnal12@gmail.com>
2024-11-28 01:14:06 +0900

    fix: key prop error

--- src/components/posts/BlogPosts.tsx
+++ src/components/posts/BlogPosts.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Fragment } from 'react';
+
 import useViewTypesTab from '@/hooks/useViewTypesTab';
 import { PostListTypes } from '@/types/common.types';
 import { cn } from '@/utils/utils';
@@ -20,23 +22,13 @@ const BlogPosts = ({ posts }: BlogPostsProps) => {
 			<ViewToggle viewType={viewType} onChangeViewType={onChangeViewType} />
 			<ul className={cn('pt-5', viewType === 'list' ? 'flex flex-col' : 'grid gap-5 sm:grid-cols-1 md:grid-cols-2')}>
 				{posts.map(post => (
-					<>
+					<Fragment key={post.frontmatter.title}>
 						{viewType === 'list' ? (
-							<PostListType
-								data={post.frontmatter}
-								category={post.category}
-								slug={post.slug}
-								key={post.frontmatter.title}
-							/>
+							<PostListType data={post.frontmatter} category={post.category} slug={post.slug} />
 						) : (
-							<PostCardType
-								data={post.frontmatter}
-								category={post.category}
-								slug={post.slug}
-								key={post.frontmatter.title}
-							/>
+							<PostCardType data={post.frontmatter} category={post.category} slug={post.slug} />
 						)}
-					</>
+					</Fragment>
 				))}
 			</ul>
 		</section>
