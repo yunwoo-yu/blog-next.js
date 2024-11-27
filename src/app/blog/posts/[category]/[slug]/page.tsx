a32994fd319dbbcb7f5ee3db27e09930d96525a0
Ycow <skypnal12@gmail.com>
2024-11-28 01:15:06 +0900

    feat: detail page meta, static params add

--- src/app/blog/posts/[category]/[slug]/page.tsx
+++ src/app/blog/posts/[category]/[slug]/page.tsx
@@ -1,11 +1,34 @@
+import markdownToTxt from 'markdown-to-txt';
+
 import CustomMDXRemote from '@/components/common/CustomMDXRemote';
 import Comments from '@/components/postDetail/Comments';
 import PostDetailHeader from '@/components/postDetail/PostDetailHeader';
-import { getPostDetail } from '@/utils/post-utils';
-
+import { getAllPosts, getAllPostsPath, getPostDetail } from '@/utils/post-utils';
 interface Params {
 	params: Promise<{ category: string; slug: string }>;
 }
+
+export const generateStaticParams = async () => {
+	const postsPaths = getAllPostsPath();
+	const posts = await getAllPosts(postsPaths);
+
+	return posts.map(post => ({ category: post.category, slug: post.slug }));
+};
+
+export const generateMetadata = async ({ params }: Params) => {
+	const { category, slug } = await params;
+	const post = await getPostDetail(category, slug);
+	const description =
+		markdownToTxt(post.source)
+			.replace(/\s{2,}/gi, ' ')
+			.slice(0, 160)
+			.trim() + '...';
+
+	return {
+		title: `${post.frontmatter.title}`,
+		description: `${description}`,
+	};
+};
 
 const PostDetailPage = async ({ params }: Params) => {
 	const { category, slug } = await params;
