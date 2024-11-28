f950665ee72c60a40563be3f1fe3c28853e03591
Ycow <skypnal12@gmail.com>
2024-11-29 04:07:12 +0900

    fix: image not found fix test 2

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
 	// Configure `pageExtensions` to include markdown and MDX files
 	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 	output: 'export',
-	unoptimized: false,
+	unoptimized: true,
 	// Optionally, add any other Next.js config below
 };
 
