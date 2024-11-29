030917107d5195a6ad311b60b1b38ad5dae0d276
Ycow <skypnal12@gmail.com>
2024-11-29 21:46:44 +0900

    test: test deploy amplify

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// Configure `pageExtensions` to include markdown and MDX files
 	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
