e7622ac39fbb4118dd89fdf993a749a789aeacc0
Ycow <skypnal12@gmail.com>
2024-11-29 21:36:53 +0900

    test: test 404 page cloudfront -3

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ import createMDX from '@next/mdx';
 const nextConfig = {
 	// Configure `pageExtensions` to include markdown and MDX files
 	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-	output: 'export',
 };
 
 const withMDX = createMDX({
