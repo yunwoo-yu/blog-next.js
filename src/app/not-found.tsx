7dc4d9f3111acce363d5aa10b6db7242d3ce1ec7
Ycow <skypnal12@gmail.com>
2024-12-04 00:18:49 +0900

    feat: add 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
+import { Button } from '@/components/ui/button';
+
 const NotFound = () => {
-	return <div>페이지를 찾을 수 없습니다.</div>;
+	return (
+		<section className="flex flex-col items-center justify-center py-20">
+			<div className="relative h-28 w-28">
+				<Image src="/images/logo.png" alt="logo" className="object-cover" fill sizes="(max-width: 1900px) 224px" />
+			</div>
+			<h2 className="prose mt-10 text-5xl font-bold dark:prose-invert">404 ERROR</h2>
+			<div className="mt-10 flex flex-col gap-3 text-center dark:prose-invert">
+				<p>페이지를 찾을 수 없습니다.</p>
+				<p>존재하지 않는 주소를 입력하셨거나,</p>
+				<p>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.</p>
+				<Button className="mt-5">
+					<Link href="/blog/posts">홈으로 가기</Link>
+				</Button>
+			</div>
+		</section>
+	);
 };
 
 export default NotFound;
