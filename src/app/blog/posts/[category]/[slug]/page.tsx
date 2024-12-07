1af646b347e67b359a6562d09099d91859e0dfde
Ycow <skypnal12@gmail.com>
2024-12-08 03:42:04 +0900

    refactor: fixed detail page layout

--- src/app/blog/posts/[category]/[slug]/page.tsx
+++ src/app/blog/posts/[category]/[slug]/page.tsx
@@ -3,7 +3,7 @@ import markdownToTxt from 'markdown-to-txt';
 import CustomMDXRemote from '@/components/common/CustomMDXRemote';
 import Comments from '@/components/postDetail/Comments';
 import PostDetailHeader from '@/components/postDetail/PostDetailHeader';
-import TitleIndex from '@/components/postDetail/TitleIndex';
+import Toc from '@/components/postDetail/Toc';
 import { getAllPosts, getAllPostsPath, getHeaderNavigationList, getPostDetail } from '@/utils/post-utils';
 interface Params {
 	params: Promise<{ category: string; slug: string }>;
@@ -18,6 +18,7 @@ export const generateStaticParams = async () => {
 
 export const generateMetadata = async ({ params }: Params) => {
 	const { category, slug } = await params;
+
 	const post = await getPostDetail(category, slug);
 	const description =
 		markdownToTxt(post.source)
@@ -34,13 +35,16 @@ export const generateMetadata = async ({ params }: Params) => {
 const PostDetailPage = async ({ params }: Params) => {
 	const { category, slug } = await params;
 	const { source, frontmatter } = await getPostDetail(category, slug);
+
 	const headerNavigationList = getHeaderNavigationList(source);
 
 	return (
 		<>
 			<PostDetailHeader frontmatter={frontmatter} />
-			<TitleIndex headerNavigationList={headerNavigationList} />
-			<section className="prose mx-auto max-w-4xl px-5 pb-20 pt-10 dark:prose-invert">
+			<article className="mx-auto max-w-3xl px-5 transition xl:sticky xl:left-[80%] xl:top-60 xl:mx-0 xl:max-w-60">
+				<Toc headerNavigationList={headerNavigationList} />
+			</article>
+			<section className="mx-auto max-w-3xl px-5 pb-20 pt-10 xl:-mt-60">
 				<CustomMDXRemote source={source} />
 				<Comments />
 			</section>
