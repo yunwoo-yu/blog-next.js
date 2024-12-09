58339b34c5e47ca31e8e5f7a6f22fbc69686d45b
Ycow <skypnal12@gmail.com>
2024-12-09 16:34:53 +0900

    refactor: Improve performance (image, font)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import createMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+	images: {
+		formats: ['image/avif', 'image/webp'], // default: ['image/webp']
+	},
 };
 
 const withMDX = createMDX({
