b3c7492229d946373fe4750dd44344c49e4a2d05
Ycow <skypnal12@gmail.com>
2025-05-20 21:54:45 +0900

    feat: 경력란 1차 완성

--- src/components/resume/Careers.tsx
+++ src/components/resume/Careers.tsx
@@ -9,8 +9,8 @@ export default function Careers() {
 		<ResumeContainer title="경력">
 			<ul className="flex flex-col gap-10">
 				{RESUME.careers.map(career => (
-					<li key={career.organization} className="flex gap-10 border-l-2 border-destructive py-2 pl-5">
-						<div className="w-52">
+					<li key={career.organization} className="flex gap-10 border-l-2 border-destructive py-2 pl-5 print:border-l">
+						<div className="w-48">
 							<h4 className="text-xl font-medium">{career.organization}</h4>
 							<p className="text-sm font-light leading-6 text-gray-500 dark:text-gray-400">{career.period}</p>
 							<p className="mt-1 text-sm">{career.position}</p>
@@ -18,22 +18,20 @@ export default function Careers() {
 						<div className="flex-1">
 							{career.projects.map(project => (
 								<div className="mb-6" key={project.title}>
-									<div className="mb-2 flex items-center">
+									<div className="mb-1 flex items-center">
 										<h5 className="text-lg font-medium">{project.title}</h5>
 										{project.links &&
 											project.links.map(link => <ResumeLink key={link.url} title={link.title} url={link.url} />)}
 									</div>
-
+									<p className="mb-2 text-xs text-gray-500 dark:text-gray-400">{project.date}</p>
 									<p className="mb-2 text-sm">{project.description}</p>
-
 									<div className="mb-3 flex flex-wrap gap-1">
 										{project.skills.map((skill, index) => (
-											<span key={index} className="rounded-md bg-gray-100 px-2 py-1 text-xs dark:bg-gray-700">
+											<span key={index} className="rounded-md bg-gray-200 px-2 py-1 text-xs dark:bg-gray-700">
 												{skill}
 											</span>
 										))}
 									</div>
-
 									<ul className="ml-5 list-disc">
 										{project.achievements.map((achievement, index) => (
 											<ResumeItem key={index} item={achievement} />
--- src/constant/resume.ts
+++ src/constant/resume.ts
@@ -30,28 +30,75 @@ export type Resume = {
 const CAREERS: Careers[] = [
 	{
 		organization: '(주) 살린',
-		position: 'I Lab | 웹 프론트엔드 개발자 (팀원)',
+		position: '웹 프론트엔드 개발자 (팀원)',
 		period: '2023.08 ~ 재직 중 (1년 9개월)',
 		projects: [
 			{
 				title: 'AI Talker',
-				description: '프로젝트 1 설명',
-				date: '',
-				skills: ['React', 'TypeScript', 'Next.js'],
+				description:
+					'OpenAI의 Assistant API를 활용한 챗봇 웹 서비스와 개인용 챗봇 커스텀을 진행할 수 있는 어드민 서비스를 개발했습니다. Next.js로 서비스를 리뉴얼하며 전체 UI, 프로젝트 설계를 진행했고, 서비스의 코어 기능인 채팅, 미니 블로그 페이지를 담당하여 개발하고 있습니다.',
+				date: '2024.12 ~ 진행 중',
+				skills: ['Next.js', 'Nest.js', 'React', 'TypeScript', 'Tailwind CSS', 'shadcn/ui', 'zustand'],
+				links: [{ title: '링크', url: 'https://www.aitalker.co.kr/aitalker' }],
 				achievements: [
 					{
-						title: '주요 업무 1',
+						title: 'SSE 방식을 활용한 스트리밍 채팅 기능 개발',
+						children: [
+							{ title: '일관적이지 않은 Markdown 메세지를 HTML로 개선하여 일관적인 UX 제공' },
+							{
+								title: '채팅 페이지 역방향 무한스크롤 UI 구현',
+								links: [
+									{
+										title: '관련 글',
+										url: 'https://www.ycow-dev.com/blog/posts/next/reverse_infinite_scroll',
+									},
+								],
+							},
+						],
 					},
 					{
-						title: '주요 업무 2',
+						title: '전체 UI 리뉴얼 및 Next.js 개발 환경 구축 및 마이그레이션',
+						links: [
+							{
+								title: '리뉴얼 전',
+								url: 'https://tarot.mirrordays.com',
+							},
+							{
+								title: '리뉴얼 후',
+								url: 'https://www.aitalker.co.kr/tarot',
+							},
+						],
+					},
+					{
+						title: '유저별 동적 메타 태그를 적용하여, 맞춤형 SEO 최적화 및 개인화된 서비스 제공',
+					},
+					{
+						title: 'ios Safari 브라우저 가상키보드 이슈 해결',
+						links: [
+							{
+								title: '관련 글',
+								url: 'https://www.ycow-dev.com/blog/posts/issues/ios_virtual_keyboards',
+							},
+						],
 					},
 				],
 			},
 			{
-				title: '프로젝트 2',
-				description: '프로젝트 2 설명',
-				skills: ['Vue.js', 'JavaScript'],
-				achievements: [{ title: '주요 업무 1' }, { title: '주요 업무 2' }],
+				title: 'AI Survey',
+				date: '2024.08 ~ 2024.11',
+				description:
+					'OpenAI의 Assistant API를 활용한 서비스 개발 MVP 프로젝트로 AI와의 대화를 통해 원하는 설문지를 생성하고 설문을 진행할 수 있는 서비스를 개발 및 유지보수를 담당하고 있습니다.',
+				skills: ['React', 'Nest.js', 'TypeScript', 'SCSS'],
+				achievements: [
+					{
+						title:
+							'AI(GPT-4o)와의 대화를 통해 원하는 설문을 자동 생성하는 MVP 서비스를 혼자서 기획·개발하여, AI 기반 서비스의 시장성과 기술적 가능성을 빠르게 검증',
+					},
+					{
+						title:
+							'프론트엔드(React) 전 영역과 서버(Nest.js)에서 OpenAI API 연동 및 스트리밍 채팅, 설문 데이터 생성 등 핵심 기능을 직접 구현',
+					},
+				],
 			},
 		],
 	},
